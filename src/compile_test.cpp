3eb5645e9a24a29c044c370a05300ffe5d1c4f71
Alejandro Mora <alejandromoramar@gmail.com>
2020-03-16 17:31:23 +0100

    Catkin package correctly set up.

---
+++ src/compile_test.cpp
@@ -0,0 +1,14 @@
+/**
+ *  scan_matching_benchmark - 3D Scan Matching Benchmark
+ *  Alejandro Mora - alejandromoramar@gmail.com
+ *
+ *  License: <>
+ */
+
+#include <ros/ros.h>
+
+int main (int argc, char* argv[])
+{
+  std::cout << "Hello World!" << std::endl;
+  return(0);
+}
